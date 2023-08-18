c9ba928ab61063248196adb12a2f413bed94e866
Sparsh Verma <sparshv70@gmail.com>
2023-08-18 09:38:29 +0530

    Minor changes

--- pages/image.tsx
+++ pages/image.tsx
@@ -1,16 +1,27 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import { useState, useEffect } from "react";
 import { storage } from "@/firebaseConfig";
-import { ref, uploadBytes, listAll, getDownloadURL, deleteObject } from "firebase/storage";
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  getDownloadURL,
+  deleteObject,
+} from "firebase/storage";
 import { v4 } from "uuid";
-
-export default function imageTester({title,page}:{title:string;
-page:string;}) {
+import { FaSpinner } from "react-icons/fa";
+export default function imageTester({
+  title,
+  page,
+}: {
+  title: string;
+  page: string;
+}) {
   const [imageUpload, setImageUpload] = useState<File>();
-  const [isUploaded,setIsUploaded] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
   const [imageList, setImageList] = useState([]);
+  const [isDeleting, setIsDeleting] = useState(false);
   const imageListRef = ref(storage, `${title}/`);
-
 
   useEffect(() => {
     setImageList([]);
@@ -22,37 +33,38 @@ page:string;}) {
         });
       });
     });
-  }, [isUploaded]);
+  }, [isUploading, isDeleting]);
 
-
-   const uploadImage = () => {
+  const uploadImage = () => {
     const name = imageUpload?.name;
     if (imageUpload === null) return;
     console.log(name);
     const imageRef = ref(storage, `${title}/${name + v4()}`);
+    setIsUploading(true);
     uploadBytes(imageRef, imageUpload).then(() => {
       alert("Image Uploaded");
       console.log("Image Uploaded");
-      setIsUploaded(true);
+      setIsUploading(false);
     });
   };
 
-  const deleteImage = (url:string) => {
+  const deleteImage = (url: string) => {
     const confirmDeletion = window.confirm("Sure wanna delete image ?");
 
-    if(!confirmDeletion) return;
-    
-    const imageRef = ref(storage,url);
+    if (!confirmDeletion) return;
 
-    deleteObject(imageRef).then(() => {
-      alert("Image Deleted");
-      setIsUploaded((prev) => !prev);
-    }).catch((err:any) => {
-      console.error(err.message);
-    })
-  }
-
-  
+    const imageRef = ref(storage, url);
+    setIsDeleting(true);
+    deleteObject(imageRef)
+      .then(() => {
+        alert("Image Deleted");
+        setIsDeleting(false);
+      })
+      .catch((err: any) => {
+        console.error(err.message);
+        setIsDeleting(false);
+      });
+  };
 
   return (
     <div>
@@ -61,28 +73,41 @@ page:string;}) {
         onChange={(event: any) => setImageUpload(event.target.files[0])}
         className=""
       />
-      <button onClick={uploadImage} className=" bg-gray-300 relative text-lg font-bold btn-primary">
+      <button
+        onClick={uploadImage}
+        className=" bg-gray-300 relative text-lg font-bold btn-primary"
+      >
         Upload
       </button>
 
-      
-        <div className="flex justify-center items-center mt-4">
-  <div className="flex flex-wrap gap-2">
-    {imageList.map((url) => (
-      <div key={url} className="flex-col p-1" style={{ width: "150px", height: "220px" }}>
-        <div className="flex justify-center items-center h-full">
-          <img src={url} alt="image" className="max-w-full max-h-full rounded-lg" />
-        </div>
-        <div className="flex justify-center">
-          <button className="hover:bg-red-800 hover:text-white" onClick={() => deleteImage(url)}>
-            Delete
-          </button>
-        </div>
+      <div className="flex mt-4">
+        {imageList.length === 0 ? (
+          isUploading || isDeleting ? (
+            <div className="flex justify-center items-center w-full h-32">
+              <FaSpinner className="text-4xl animate-spin" />
+            </div>
+          ) : (
+            <p>No images available.</p>
+          )
+        ) : (
+          imageList.map((url) => (
+            <div key={url} className="flex-col p-1" style={{ width: "150px", height: "220px" }}>
+              <div className="flex justify-center items-center h-full">
+                <img src={url} alt="image" className="max-w-full max-h-full" />
+              </div>
+              <div className="flex justify-center mt-2">
+                <button
+                  className="hover:bg-red-800 hover:text-white"
+                  onClick={() => deleteImage(url)}
+                  disabled={isDeleting}
+                >
+                  {isDeleting ? <FaSpinner className="animate-spin" /> : "Delete"}
+                </button>
+              </div>
+            </div>
+          ))
+        )}
       </div>
-    ))}
-  </div>
-</div>
-
-      </div>
+    </div>
   );
 }
